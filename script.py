2e45154232a9b6226553222c834aa17cf319e5b3
melistraza <melistraza@gmail.com>
2018-10-28 10:33:11 +0200

    step_1: change API method get all updates from last call

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import requests
 
 TOKEN = 'your_token'
 
-API_URL = f'https://api.telegram.org/bot{TOKEN}/getMe'
+API_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 
 request = requests.get(API_URL)
 
