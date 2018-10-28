8bb721039247bb1ad3c08cd8caa082b3945d704b
melistraza <melistraza@gmail.com>
2018-10-28 18:50:31 +0200

    step_6 add inline handler

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import telebot
+from telebot import types
 from telebot.types import Message
 
 TOKEN = 'your_token'
@@ -23,4 +24,14 @@ def reply_message(message: Message):
     bot.reply_to(message, 'Bot response: {}'.format(message.text))
 
 
-bot.polling()
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
+
+
+bot.polling(timeout=60)
