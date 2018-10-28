ea21486b9fa4284f49e57ad4185e6b1df9e8935d
melistraza <melistraza@gmail.com>
2018-10-28 12:29:01 +0200

    step_5 add command handlers (start, help) add python annotate change bot text message reply

--- script.py
+++ script.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 import telebot
+from telebot.types import Message
 
-bot = telebot.TeleBot('your_token')
+TOKEN = 'your_token'
+
+bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(content_types=['text'])
-def reply_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start'])
+def send_start(message: Message):
+    bot.reply_to(message, "This is test bot.")
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message: Message):
+    bot.send_message(message.chat.id, "Try to figure out by yourself!")
+
+
+@bot.edited_message_handler(content_types=['text'], func=lambda message: True)
+@bot.message_handler(content_types=['text'], func=lambda message: True)
+def reply_message(message: Message):
+    bot.reply_to(message, 'Bot response: '.format(message.text))
 
 
 bot.polling()
